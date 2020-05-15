f64bf32ca65e3d5a58a88617d876e26eb0c42049
Gustavo Romero <gromero@tutanota.com>
2020-05-15 14:40:58 -0300

    Add interactive dep. menu

--- server.R
+++ server.R
@@ -1,7 +1,31 @@
 library(shiny)
-# source("simula.R")
+library(stringr)
 
-shinyServer(function(input, output) {
-  output$table <- renderTable( dataset[dataset$MUNICÍPIOS==input$cidade,], rownames=TRUE)
-  }
-)
+prefix <- "arranjo_.*"
+dir_files <- drop_dir("/")$name
+db_files <- str_extract(dir_files, prefix)[!is.na(str_extract(dir_files,prefix))]
+db_years <- gsub(".*([0-9]{4}).csv$","\\1", db_files)
+
+shinyServer(
+
+function(input, output) {
+  output$ano_selector <- renderUI({ selectInput("ano","Ano:", choices=db_years, selected="1970") })
+  output$estado_selector <- renderUI({
+#   print(input$ano);
+    # This is a workaround since "ano" might hit here being NULL when ano_selector
+    # is finishing render, so avoid it, otherwise filename to download is crap and
+    # a HTTP 409 error happens.
+    if (!is.null(input$ano)) {
+#     drop_download(paste("arranjo_BR_",input$ano,".csv",sep=""),overwrite=TRUE)
+#     db <<- read.csv(paste("./arranjo_BR_",input$ano,".csv",sep=""))
+      db <<- drop_read_csv(paste("/arranjo_BR_",input$ano,".csv",sep=""))
+      estados <- levels(db[which(db$CENSO==input$ano),]$ESTADO)
+      selectInput("estado","Estado:", choices=estados)
+    }
+  })
+  output$cidade_selector <- renderUI({
+    cidades <- db[which(db$CENSO==input$ano & db$ESTADO==input$estado),]$MUNICIPIO
+    selectInput("cidade","Cidade:", choices=as.vector(cidades))
+  })
+  output$table <- renderTable( db[which(db$CENSO==input$ano & db$ESTADO==input$estado & db$MUNICIPIO==input$cidade),], rownames=TRUE )
+})
--- ui.R
+++ ui.R
@@ -1,25 +1,15 @@
 library(shiny)
 library(rdrop2)
 
-datafile <- drop_dir()$path_display[2] # dataset_SP_1970.csv
-dataset <- drop_read_csv(datafile)
-cidades <- list("SP" = "São Paulo", "MG" = "Minas Gerais")
-
-mun <- levels(dataset$MUNICÍPIOS)[]
-lista_mun <- as.list(mun)
-names(lista_mun) <- mun
-
 shinyUI(fluidPage(
   titlePanel("Arranjo Domiciliar"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("cidade", "Cidade", choices=lista_mun),
+      uiOutput("ano_selector"),
+      uiOutput("estado_selector"),
+      uiOutput("cidade_selector"),
       hr()),
-     mainPanel(tableOutput("table"))
+    mainPanel(tableOutput("table"))
   )
  )
 )
-#  server = function (input, output) {
-#   output$data <- renderTable( {
-#     dataset[dataset$MUNICÍPIOS==c("ADAMANTINA"),]}, rownames=TRUE)
-#   }
