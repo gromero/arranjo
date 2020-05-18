b1b949b9a4cc27de15ad195abd6effc18a7e9557
Gustavo Romero <gromero@tutanota.com>
2020-05-18 12:10:07 -0300

    Adjust to new database

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(stringr)
 
-prefix <- "arranjo_.*"
+prefix <- "ARRANJOS_DOMICILIARES_BR_.*"
 dir_files <- drop_dir("/")$name
 db_files <- str_extract(dir_files, prefix)[!is.na(str_extract(dir_files,prefix))]
 db_years <- gsub(".*([0-9]{4}).csv$","\\1", db_files)
@@ -16,9 +16,11 @@ function(input, output) {
     # is finishing render, so avoid it, otherwise filename to download is crap and
     # a HTTP 409 error happens.
     if (!is.null(input$ano)) {
-#     drop_download(paste("arranjo_BR_",input$ano,".csv",sep=""),overwrite=TRUE)
+      db_name <- paste("ARRANJOS_DOMICILIARES_BR_", input$ano, ".csv", sep="")
+      drop_download(db_name, overwrite=TRUE)
+      db <<- read.csv(db_name, sep=";", fileEncoding="latin1")
 #     db <<- read.csv(paste("./arranjo_BR_",input$ano,".csv",sep=""))
-      db <<- drop_read_csv(paste("/arranjo_BR_",input$ano,".csv",sep=""))
+#     db <<- drop_read_csv(paste("ARRANJOS_DOMICILIARES_BR_",input$ano,".csv",sep=""), fileEncoding="latin1")
       estados <- levels(db[which(db$CENSO==input$ano),]$ESTADO)
       selectInput("estado","Estado:", choices=estados)
     }
